00e91af8dc1f6f24b2e40e5d986c3e227a320991
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-25 11:13:50 +0100

    imgs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
+import Header from '../components/Header/index'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -8,6 +10,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <Header />
      <h1>SUPPPPP</h1>
     </main>
   )
