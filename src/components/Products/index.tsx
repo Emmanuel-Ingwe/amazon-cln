6afda92c44608adb5f967f82cf705a6c531dbddc
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-27 12:05:53 +0100

    -

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { ProductProps } from '../../../type'
+import Image from 'next/image'
 
 type Props = {}
 
 const index = ({ productData }: any) => {
     console.log(productData)
   return (
-    <div>
+    <div className='w-full px-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6'>
         {productData.map(({_id,title,brand,category,description,image,isNew,oldPrice,price }: ProductProps) => (
-            <p>{title}</p>
+            <div key={_id}> 
+                <Image width={300} height={300} src={image} alt='productImage'
+                />
+            </div>
         ))}
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,9 @@ export default function Home({ productData }: Props) {
     <main>
       <div className="max-w-screen-2xl mx-auto">
         <Banner />
-        <Products productData={productData} />
+        <div className="relative md:-mt-20 lgl:-mt-32 xl:-mt-60 z-20 mb-10">
+          <Products productData={productData} />
+        </div>
       </div>
     </main>
   )
