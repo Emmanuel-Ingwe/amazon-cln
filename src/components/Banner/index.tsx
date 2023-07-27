c4f52d123a1e2fded202bcff8e76fc815a76ab54
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-27 11:15:04 +0100

    banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { Carousel } from "react-responsive-carousel"
+import sliderImg_1 from "../../images/sliderImg_1.jpg"
+import sliderImg_2 from "../../images/sliderImg_2.jpg"
+import sliderImg_3 from "../../images/sliderImg_3.jpg"
+import Image from "next/image"
 
 type Props = {}
 
 const index = (props: Props) => {
   return (
-    <div>index</div>
+    <div className="">
+        <Carousel>
+    <div>
+        <Image src={sliderImg_1} alt="slider1" />
+    </div>
+    <div>
+        <Image src={sliderImg_2} alt="" />
+    </div>
+    <div>
+        <Image src={sliderImg_3}alt="" />
+    </div>
+    </Carousel>
+    </div>
   )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import RootLayout from "@/components/RootLayout"
 import type { AppProps } from 'next/app'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
