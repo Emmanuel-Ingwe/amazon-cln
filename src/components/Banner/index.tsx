324eed28032c9381feb3e708cd55e2244e458343
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-27 11:38:30 +0100

    banner/ priorities

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,17 +1,17 @@
 import { Carousel } from "react-responsive-carousel"
-import sliderImg_1 from "../../images/sliderImg_1.jpg"
-import sliderImg_2 from "../../images/sliderImg_2.jpg"
-import sliderImg_3 from "../../images/sliderImg_3.jpg"
+import sliderImg_1 from "../../images/slider/sliderImg_1.jpg"
+import sliderImg_2 from "../../images/slider/sliderImg_2.jpg"
+import sliderImg_3 from "../../images/slider/sliderImg_3.jpg"
 import Image from "next/image"
 
 type Props = {}
 
 const index = (props: Props) => {
   return (
-    <div className="">
-        <Carousel>
+    <div className="relative">
+        <Carousel className="relative" autoPlay infiniteLoop showStatus={false} showIndicators={false} showThumbs={false} interval={3001}>
     <div>
-        <Image src={sliderImg_1} alt="slider1" />
+        <Image priority src={sliderImg_1} alt="slider1" />
     </div>
     <div>
         <Image src={sliderImg_2} alt="" />
@@ -20,6 +20,7 @@ const index = (props: Props) => {
         <Image src={sliderImg_3}alt="" />
     </div>
     </Carousel>
+    <div className="w-full h-40 bg-gradient-to-t from-gray-100 to-transparent absolute bottom-0 z-20"></div>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Banner from "@/components/Banner"
+import Products from "@/components/Products"
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ productData }) {
+  console.log(productData)
   return (
     <main>
       <div className="max-w-screen-2xl mx-auto">
         <Banner />
+        <Products />
       </div>
     </main>
   )
+}
+
+
+export const getServerSideProps = async () => {
+  const res = await fetch("https://fakestoreapiserver.reactbd.com/tech")
+  const productData = await res.json()
+  return { props: { productData } }
 }
