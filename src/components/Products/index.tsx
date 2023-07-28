addbaf30d2d712f88883afb9e37617bb7319d4e0
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-28 23:59:30 +0100

    prod. class

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -9,7 +9,7 @@ const index = ({ productData }: any) => {
   return (
     <div className='w-full px-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6'>
         {productData.map(({_id,title,brand,category,description,image,isNew,oldPrice,price }: ProductProps) => (
-            <div key={_id}> 
+            <div key={_id} className='w-full bg-white'> 
                 <Image width={300} height={300} src={image} alt='productImage'
                 />
             </div>
