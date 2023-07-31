4aab0453b4751797c07f1b7d74fc2bca66671eac
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-31 01:32:04 +0100

    store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,17 @@ import '@/styles/globals.css'
 import RootLayout from "@/components/RootLayout"
 import type { AppProps } from 'next/app'
 import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Provider } from 'react-redux'
+import { store } from '@/store/store'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="font-bodyfont bg-gray-300">
-      <RootLayout>
-         <Component {...pageProps} />
-      </RootLayout>
-    </div>
+    <Provider store={store}>
+      <div className="font-bodyfont bg-gray-300">
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+      </div>
+    </Provider>
   )
 }
