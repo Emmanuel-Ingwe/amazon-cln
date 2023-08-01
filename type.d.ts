e27834c4258b0db1867d42f89a7702b769c91872
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-08-01 06:03:17 +0100

    type prodstore

--- type.d.ts
+++ type.d.ts
@@ -9,3 +9,16 @@ export interface ProductProps{
     title:string;
     _id:number;
 }
+
+export interface StoreProduct {
+    brand: string;
+    category:string;
+    description:string;
+    image:string;
+    isNew:string;
+    oldPrice:number;
+    price:string;
+    title:string;
+    _id:number;
+    quantity: number;
+}
