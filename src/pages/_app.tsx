78e7ccebf89d8803292fb18da8ca430d326c3f36
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-08-06 07:39:38 +0100

    add: next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,27 @@
-import '@/styles/globals.css'
-import RootLayout from "@/components/RootLayout"
-import type { AppProps } from 'next/app'
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import { Provider } from 'react-redux'
-import { store } from '@/store/store'
+import RootLayout from "@/components/RootLayout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Provider } from "react-redux";
+import { persistor, store } from "@/store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <Provider store={store}>
-      <div className="font-bodyfont bg-gray-300">
-        <RootLayout>
-          <Component {...pageProps} />
-        </RootLayout>
-      </div>
+      <PersistGate persistor={persistor} loading={null}>
+        <SessionProvider session={session}>
+          <div className="font-bodyFont bg-gray-300">
+            <RootLayout>
+              <Component {...pageProps} />
+            </RootLayout>
+          </div>
+        </SessionProvider>
+      </PersistGate>
     </Provider>
-  )
+  );
 }
