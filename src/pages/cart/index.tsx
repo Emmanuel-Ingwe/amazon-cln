d803d35b42bb07fc79e70588128814b9a3db250e
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-08-13 14:26:48 +0100

    add: cart text colr

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -15,9 +15,13 @@ const cart = (props: Props) => {
     <div className='max-w-screen-2xl mx-auto px-6 grid grid-cols-5 gap-10 py-4'>
       {
         productData.length > 0 ?(
-          <div>
-
+          <>
+          <div className='bg-white col-span-4 p-4 rounded-lg'>
+            <div className='flex items-center justify-between border-b-[1px] border-b-gray-400 pb-1'>
+              <p className='text-2xl font-semibold text-amazon_blue'>Shopping cart</p>
+            </div>
           </div>
+          </>
         ) : (
           <div className="">
             <h1 className="">Your cart is empty</h1>
@@ -29,4 +33,4 @@ const cart = (props: Props) => {
   )
 }
 
-export default cart
+export default cart  
