f0a21dc0b7ea59ada0dba89de19952fba8971639
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-08-19 00:25:04 +0100

    add: btn to resetcart

--- src/components/ResetCart/index.tsx
+++ src/components/ResetCart/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { resetCart } from "@/store/nextSlice";
+import { useDispatch } from "react-redux";
 
-type Props = {}
-
-function index({}: Props) {
+const ResetCart = () => {
+  const dispatch = useDispatch();
+  const handleResetCart = () => {
+    const confirmReset = window.confirm(
+      "Are you sure to reset your items from the cart?"
+    );
+    if (confirmReset) {
+      dispatch(resetCart());
+    }
+  };
   return (
-    <div>index</div>
-  )
-}
+    <button
+      onClick={handleResetCart}
+      className="w-44 h-10 font-semibold bg-gray-200 rounded-lg hover:bg-red-600 hover:text-white duration-300"
+    >
+      reset cart
+    </button>
+  );
+};
 
-export default index
+export default ResetCart;
