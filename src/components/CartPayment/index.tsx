72c1d32b7bbc0484b75d6f885faa404081428685
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-08-21 01:40:46 +0100

    payment cart

--- src/components/CartPayment/index.tsx
+++ src/components/CartPayment/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { SiMediamarkt } from "react-icons/si";
 
 type Props = {}
 
 const index = (props: Props) => {
   return (
-    <div>index</div>
+    <div>
+      <div className="flex gap-2">
+        <span className="">
+          <SiMediamarkt />
+        </span>
+      </div>
+    </div>
   )
 }
 
