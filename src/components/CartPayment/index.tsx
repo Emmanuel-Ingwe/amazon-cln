2680949e8a681ffdb7fd77fb84507429beac4518
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-08-21 01:44:17 +0100

    !!

--- src/components/CartPayment/index.tsx
+++ src/components/CartPayment/index.tsx
@@ -5,11 +5,14 @@ type Props = {}
 
 const index = (props: Props) => {
   return (
-    <div>
+    <div className='flex flex-col gap-4'>
       <div className="flex gap-2">
-        <span className="">
+        <span className="bg-green-600 rounded-full p-1 h-6 w-6 text-sm text-white flex items-center justify-center mt-1">
           <SiMediamarkt />
         </span>
+        <p className="text-sm">
+          Your order qualifies for FREE Shipping bt choosing this option at checkout. see datails....
+        </p>
       </div>
     </div>
   )
